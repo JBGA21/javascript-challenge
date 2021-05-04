e7d8432fe3eed404eba8e65d6492c0641ae73727
JBGA21 <jesusbeltran_93@hotmail.com>
2021-05-04 17:19:10 -0500

    Level 1 done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,12 +18,26 @@ var button = d3.select("#filter-btn");
 
 button.on("click", function () {
     tableBody.html("");
+    // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
+    // get the value property of the input element
     var inputValue = inputElement.property("value");
+    // Print the value to the console
     console.log(inputValue);
+
     var filterData = tableData.filter(report => report.datetime === inputValue);
     console.log(filterData);
-})
+
+    filterData.forEach(function (dateData) {
+        console.log(dateData);
+        var row = tableBody.append("tr");
+        Object.entries(dateData).forEach(function ([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
 
 //}
 
